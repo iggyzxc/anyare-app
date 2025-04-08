8a72badde0c4583f6eb587605195f8887699fc5b
Iggy <ignaciomacam@gmail.com>
2025-04-08 18:29:13 +0800

    Initial commit: Spring Boot project structure

---
+++ src/main/java/com/iggyzxc/anyareappbackend/AnyareAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.iggyzxc.anyareappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyareAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnyareAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iggyzxc/anyareappbackend/AnyareAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iggyzxc.anyareappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnyareAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
